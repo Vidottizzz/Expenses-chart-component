c995d14125898a109edad620d1b1cc17746a8864
Vidotti <vidottizzz@gmail.com>
2023-04-11 14:24:02 -0300

    highest value different color Chart.js

--- script/script.js
+++ script/script.js
@@ -1,88 +1,87 @@
-// google.charts.load("current", { packages: ["corechart"] });
-// google.charts.setOnLoadCallback(drawChart);
+let myBalance = document.querySelector("#myBalance");
+console.log(myBalance.innerHTML);
+let monthTotal = document.querySelector("#monthTotal");
+console.log(monthTotal.innerHTML);
+let lastMonthTotal = document.querySelector("#lastMonthTotal");
+console.log(lastMonthTotal.innerHTML);
+//função gerar random number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+// valores randomicos no array para ficar mais dinamico
+let valores = [
+  getRandomInt(100),
+  getRandomInt(100),
+  getRandomInt(100),
+  getRandomInt(100),
+  getRandomInt(100),
+  getRandomInt(100),
+  getRandomInt(100),
+];
+// dando fetch automaticamente ja colocando os dados dentro do chart
+fetch("data.json").then((response) => {
+  response.json().then((dados) => {
+    let usuariosArr = [];
+    let usuariosAmount = [];
+    console.log(dados.usuarios);
+    dados.usuarios.map((usuario) => {
+      usuariosArr.push(usuario.day);
+      usuariosAmount.push(usuario.amount);
+    });
+    // desenhando o chart , no caso dentro do fetch para utilizar os dados extraidos do JSON
+    const ctx = document.getElementById("myChart");
+    
+    let backgroundcolor = [];
 
-// function drawChart() {
-//   var data = google.visualization.arrayToDataTable([
-//     ["Element", "Density", { role: "style" }],
-//     ["mon", 8.94, "#ec775f"],
-//     ["tue", 10.49, "#ec775f"],
-//     ["wed", 19.3, "#ec775f"],
-//     ["thu", 19.3, "#ec775f"],
-//     ["fri", 19.3, "#ec775f"],
-//     ["sat", 19.3, "#ec775f"],
-//     ["sun", 21.45, "#76b5bc"],
-// ]);
+    const max = Math.max(...valores);
+    console.log(max);
+    const highestValueColor = valores.map((valor, index) => {
+      // console.log(valor, index);
+      const color = valor === max ? 'hsl(186, 34%, 60%)' : 'hsl(10, 79%, 65%)';
+      // console.log(color);
+      backgroundcolor.push(color);
+    });
+    console.log(backgroundcolor)
 
-// var view = new google.visualization.DataView(data);
-// view.setColumns([
-//     0,
-//     1,
-//     { calc: "stringify", type: "string", role: "annotation" },
-//     2,
-// ]);
-// var options = {
-
-//         width: 340,
-//         height: 300,
-//     bar: { groupWidth: "20%", gap:"20%"},
-//     legend: { position: "none" },
-//     vAxis: {
-//       gridlines: {
-//         interval: 0,
-//         color: 'transparent',
-//         count : 0
-//       },
-//     },
-//   };
-//   var chart = new google.visualization.ColumnChart(
-//     document.getElementById("columnchart_values")
-//   );
-//   chart.draw(view, options);
-// }
-
-const ctx = document.getElementById("myChart");
-
-let labelsX = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-let valores = [10, 20, 30, 40, 50, 60, 70];
-new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: labelsX,
-    datasets: [
-      {    
-        label: '',
-        data: valores,
-        backgroundColor: "hsl(10, 79%, 65%)",
-        borderRadius: 5, 
-        barThickness: 32,
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: usuariosArr,
+        datasets: [
+          {
+            label: "percentage spend",
+            //podia ter colocado no data : usuariosAmount ( quer seria os dados do JSON)
+            data: valores,
+            backgroundColor:backgroundcolor,
+            borderRadius: 5,
+            barThickness: 32,
+          },
+        ],
       },
-    ],
-  },
-  options: {
-    plugins: {
-        legend: {
-            display: false // This hides all text in the legend and also the labels.
-        }
-    },
-    scales: {
-        
-              x: {
-                
-        grid: {
-            display: false,
-        }
-    },
-    y: {
-        display: false,
-        grid: {
-            display: false,
+      options: {
+        plugins: {
+          legend: {
+            display: false, // This hides all text in the legend and also the labels.
+          },
         },
-        beginAtZero: true,
-       ticks : {
-        display: false,
-       }
+        scales: {
+          x: {
+            grid: {
+              display: false,
+            },
+          },
+          y: {
+            display: false,
+            grid: {
+              display: false,
+            },
+            beginAtZero: true,
+            ticks: {
+              display: false,
+            },
+          },
+        },
       },
-      
-    },
-  },
+    });
+  });
 });
